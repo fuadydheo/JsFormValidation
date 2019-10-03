7f46606e177f50f9c1d30106b04eec9db57e8430
fuadydheo <fuadydheo@gmail.com>
2019-10-03 21:02:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,29 @@
+function validation(){
+    var nama = document.getElementById("nama").value;
+    var email = document.getElementById("email").value;
+    var address = document.getElementById("address").value;
+    var city = document.getElementById("city").value;
+    var country = document.getElementById("country").value;
+    var sex = document.querySelector('input[name="sex"]:checked').value;
+    var berlangganan = document.getElementById("berlangganan").checked;
+
+    if(nama == ""){
+        document.getElementById('namaid').innerHTML = " tolong isi namanya!";
+        return false;
+    }
+
+
+    var json = {
+        name: nama,
+        email: email,
+        address: address,
+        city: city,
+        country: country,
+        sex: sex,
+        subscription: berlangganan         
+    }
+    console.log(json);
+
+
+   // return false;
+}
